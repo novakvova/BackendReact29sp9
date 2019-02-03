f04f13d8759a33c4e9641ecae8df57d5126fb945
novakvova <novakvova@gmail.com>
2019-02-03 11:07:34 +0200

    Begin webapi

---
+++ Backend/Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Backend.Controllers
+{
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+    public class GameController : ApiController
+    {
+        // GET: Game
+        public IHttpActionResult GetAllGames()
+        {
+            var model = new List<GameItemViewModel>
+            {
+                new GameItemViewModel
+                {
+                    id=1,
+                    title="GTA 5",
+                    image="https://torrent-igruha.org/uploads/posts/2015-02/1424891927_grand_theft_auto_v_gta_5-16989099-frntl.jpg"
+                },
+                new GameItemViewModel
+                {
+                    id=2,
+                    title="DOTA 2",
+                    image="https://static-cdn.jtvnw.net/ttv-boxart/Dota%202.jpg"
+                }
+            };
+
+            return Content(HttpStatusCode.OK, model);
+        }
+    }
+}
---
+++ Backend/Models/GameViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class GameItemViewModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+    }
+}
